8f2f7debc3a4a7c794ae2c42ff40346ae8321017
Ivan <ivanrs297@gmail.com>
2020-03-10 22:17:56 -0600

    Adding client Raspberry Sockets 2.0

---
+++ sockets2.0/client_rasp.py
@@ -0,0 +1,128 @@
+import socket 
+import time
+import datetime
+import pymongo
+from pymongo import MongoClient
+import cloudinary
+import cloudinary.uploader
+import os
+import webbrowser
+import RPi.GPIO as GPIO
+from picamera import PiCamera
+
+
+# Globals:
+in_connection = False
+
+# DB Config:
+connection_params = {
+    'user': 'cinv',
+    'password': "cinv123",
+    'host': 'ds037175.mlab.com',
+    'port': 37175,
+    'namespace': 'cinvestav',
+}
+
+connection = MongoClient(
+    'mongodb://{user}:{password}@{host}:'
+    '{port}/{namespace}?retryWrites=false'.format(**connection_params)
+)
+
+db = connection.cinvestav
+db = db["schedule"]
+
+cloudinary.config(
+  cloud_name = 'dv0bco9rw',  
+  api_key = '621813665726586',  
+  api_secret = '5zNWkLE3ii-a1MrmC7jziLv6WOE'  
+)
+
+def upload_img_to_cloudinary(img_name):
+	cloudinary.uploader.upload(img_name, public_id = "oscar")
+
+def get_img_from_cloudinary(img_name):
+	return cloudinary.utils.cloudinary_url(img_name)
+
+
+def get_schedule():
+	schedules = db.find()
+	return schedules
+
+def insert_data_to_db():
+	db.insert_many([
+		{"start": "8.50", "finish": "11", "day": "Monday", "class": "Distributed Systems", "professor": "Dr. Felix Corchado"},
+		{"start": "8.50", "finish": "11", "day": "Wednesday", "class": "Distributed Systems", "professor": "Dr. Felix Corchado"},
+		{"start": "8.50", "finish": "11", "day": "Friday", "class": "Distributed Systems", "professor": "Dr. Felix Corchado"},
+	])
+
+# Function to create the client message
+def create_client_msg(HOST, PORT, msg):	
+	print(f"Connecting to server: {HOST} on port: {PORT}")
+	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sck:
+		try:
+			sck.connect((HOST, PORT))
+		except Exception as e:
+			raise SystemExit(f"We have failed to connect to host: {HOST} on port: {PORT}, because: {e}")
+
+		sck.sendall(msg.encode('utf-8'))
+		
+		data = sck.recv(1024)
+		if data:
+			print(f"RES: {data.decode()}")
+
+# Function send command of open PDF
+def open_pdf(pdf_name):
+	return f"open pdf {pdf_name}"
+
+def is_time_of_assignement():
+	now = datetime.datetime.now()
+	assignatures_schedule = get_schedule()
+	for a in assignatures_schedule:
+		if now.today().strftime("%A") == a["day"] and now.hour >= float(a["start"]) and now.hour <= float(a["finish"]):
+		# if now.today().strftime("%A") == a["day"]:
+			print(f"Assignature: {a['class']}" )
+			return True
+	return False
+
+while False:
+	time.sleep(3)
+
+	# If its time of a assignature, then show de PDF presentation
+	if not in_connection and is_time_of_assignement() :
+		in_connection = True
+		create_client_msg(socket.gethostname(), 4000, open_pdf("present"))
+
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(16, GPIO.IN)
+camera = PiCamera()
+camera.vflip = None
+a=0
+while True:
+	i = GPIO.input(16)
+	if i == 0:
+		print("No intruders", i)
+		#camera.stop_preview()
+		time.sleep(.3)
+		a=0
+	elif i == 1:
+		print("Intruders detected", i)
+		       
+		time.sleep(.3)
+		a=a+1
+		if a==1:
+			#camera.start_preview() 
+			camera.capture('/home/pi/Desktop/image.jpg')
+			#upload_img_to_cloudinary('/home/pi/Desktop/image.jpg')
+
+		
+
+#img = get_img_from_cloudinary("anime.jpeg")
+#img = f'{img[0]}.jpeg'
+#webbrowser.open(img)  # Go to example.com
+
+
+
+
+
