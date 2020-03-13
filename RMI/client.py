8663943ef3b54fbafdf60cf9ff6fc5b424502d62
Ivan Reyes <ivanrs297@gmail.com>
2020-03-13 17:53:20 -0600

    Read from SERIAL port

--- RMI/client.py
+++ RMI/client.py
@@ -1,16 +1,19 @@
 from datetime import datetime
-
+import serial, time
 import Pyro4
+
+arduino = serial.Serial('/dev/ttyACM0', 9600)
+
 
 server = Pyro4.Proxy('PYRO:monoide.server@10.0.5.239:4000')
 
 def send():
-	now = datetime.now()
-	server.change_temp('27', '1', datetime.now())
-	print(f'sent at {now:%H:%M:%S} \n')
+    now = datetime.now()
+    rawString = arduino.readline()  # Get String from Serial Port
+    classroom_id = 1
+    server.change_temp(rawString.decode('utf-8'), classroom_id, datetime.now())
 
 try:
-	print("hola")
-	send()
+    send()
 except (KeyboardInterrupt, EOFError):
-	print('Goodbye! (:')
+    print('Closing! (:')
